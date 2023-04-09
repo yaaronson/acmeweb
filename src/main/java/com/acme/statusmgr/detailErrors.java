c08bd9c3727704c82b89170bd275b8c28534ba12
yaaronso <yaaronso@student.touro.edu>
2023-04-09 19:21:13 -0400

    story #4 - made a seperate class to handle invalid detail types and output proper error message

---
+++ src/main/java/com/acme/statusmgr/detailErrors.java
@@ -0,0 +1,24 @@
+package com.acme.statusmgr;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason="To show an example of a custom message")
+class detailError extends RuntimeException{
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public detailError(String message) {
+        super("Invalid details option: " + message);
+    }
+
+    public detailError() {
+
+    }
+}
